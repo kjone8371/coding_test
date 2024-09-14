d82b0f9cf148ff656484db1bf60f451ae0522cfb
kjone8371 <kjone8371@gmail.com>
2024-09-14 18:02:24 +0900

    소대문자를 바꿔서 출력 하기 프로그래머스

---
+++ src/알고리즘_테스트/repeat.java
@@ -0,0 +1,32 @@
+package 알고리즘_테스트;
+
+import java.util.Scanner;
+
+public class repeat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+//        String str = sc.next();
+//        int n = sc.nextInt();
+//        for(int i = 0; i <= n; i++){
+//            System.out.print(str);
+//        }
+
+//        System.out.println(str.repeat(n));
+
+
+        String answer = "";
+
+       for(int i = 0; i < a.length(); i++){
+           char c = a.charAt(i);
+           if(Character.isUpperCase(c)){
+               answer += Character.toLowerCase(c);
+           }else {
+               answer += Character.toUpperCase(c);
+           }
+       }
+
+        System.out.println(answer);
+    }
+}
