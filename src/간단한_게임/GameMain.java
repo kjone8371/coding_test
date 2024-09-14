82314d8a66e4017e8208c8e632a724a8e082d399
kjone8371 <kjone8371@gmail.com>
2024-09-14 13:47:40 +0900

    게임 메인

---
+++ src/간단한_게임/GameMain.java
@@ -0,0 +1,8 @@
+package 간단한_게임;
+
+public class GameMain{
+    public static void main(String[] args) {
+        GameController controller = new GameController();
+        controller.startGame();
+    }
+}
