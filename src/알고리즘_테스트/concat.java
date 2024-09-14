4c0f830660c08d99201d4f5eedefe83d7ee3f31f
kjone8371 <kjone8371@gmail.com>
2024-09-14 19:09:44 +0900

    문자열 붙여서 출력 프로그래머스

---
+++ src/알고리즘_테스트/concat.java
@@ -0,0 +1,12 @@
+package 알고리즘_테스트;
+
+import java.util.Scanner;
+
+public class concat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a+b);
+    }
+}
