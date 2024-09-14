c565e71b33d3c8d6a14c590dbf9b3d31bb807e01
kjone8371 <kjone8371@gmail.com>
2024-09-14 13:51:36 +0900

    프린트 출력 문제1

---
+++ src/알고리즘_테스트/printf.java
@@ -0,0 +1,13 @@
+package 알고리즘_테스트;
+
+public class printf {
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = String.valueOf(val1);
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
