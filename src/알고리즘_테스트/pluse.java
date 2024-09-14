12d4589296a8b8b734a640b45688a32f6e278246
kjone8371 <kjone8371@gmail.com>
2024-09-14 19:07:20 +0900

    덧셈 뺄쎔 프로그래머스

---
+++ src/알고리즘_테스트/pluse.java
@@ -0,0 +1,15 @@
+package 알고리즘_테스트;
+
+import java.util.Scanner;
+
+public class pluse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+        sum = a + b;
+
+        System.out.println(a + " + " + b + " = " + sum);
+    }
+}
