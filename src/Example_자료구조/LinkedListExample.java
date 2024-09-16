94f78e338c28b9a07c1fae75d36df9513db83f9c
kjone8371 <kjone8371@gmail.com>
2024-09-16 22:32:43 +0900

    연결리스트 예시문

--- src/Example_자료구조/LinkedListExample.java
+++ src/Example_자료구조/LinkedListExample.java
@@ -9,7 +9,8 @@ class Node {
     }
 }
 //연결리스트
-//연결리스트는 요소들이 노드로 구성되며, 각 노드는 데이터와 다음 노드의 주소를 가집니다. 크기가 가변적이라는 장점이 있습니다.
+//연결리스트는 요소들이 노드로 구성되며, 각 노드는 데이터와 다음 노드의 주소를 가집니다.
+// 크기가 가변적이라는 장점이 있습니다.
 public class LinkedListExample {
     public static void main(String[] args) {
         Node head = new Node(1);
