00cf5d710e74238cf590e245b3e44d1f07371df7
kjone8371 <kjone8371@gmail.com>
2024-09-17 20:02:33 +0900

    아직 이해 못함 공부 더 해야 함

--- src/프로그래머스/최소직사각형.java
+++ src/프로그래머스/최소직사각형.java
@@ -3,23 +3,29 @@ package 프로그래머스;
 public class 최소직사각형 {
 
     public static int solution(int[][] sizes) {
-        int maxWidth = 0;
-        int maxHeight = 0;
-
-        // 각 명함의 가로와 세로 길이를 비교하여 최대 가로 길이와 최대 세로 길이를 결정
-        for (int[] size : sizes) {
-            int width = Math.min(size[0], size[1]);
-            int height = Math.max(size[0], size[1]);
-            maxWidth = Math.max(maxWidth, width);
-            maxHeight = Math.max(maxHeight, height);
+        int length = 0, height = 0;
+        for (int[] card : sizes) {
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
         }
+        int answer = length * height;
+        return answer;
 
-        // 최대 가로 길이와 최대 세로 길이를 한 값을 리턴
-        return maxWidth * maxHeight;
+
+    }
+
+    public static int test(int[][] sizes){
+        int length = 0, height = 0;
+        for(int [] card : sizes){
+            length = Math.max(length, Math.max(card[0], card[1]));
+            height = Math.max(height, Math.min(card[0], card[1]));
+        }
+        int answer = length * height;
+        return answer;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(solution(new int[][]{{60, 50}, {30, 70}, {60, 30}, {80, 40}}));
+        System.out.println(test(new int[][]{{60, 50}, {30, 70}, {60, 30}, {80, 40}}));
     }
 }
