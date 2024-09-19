732b6f3fff92bead203f00f273d099fcc58a78b0
kjone8371 <kjone8371@gmail.com>
2024-09-19 14:45:23 +0900

    2차원 배열 테스트

---
+++ src/Array.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+
+
+public class Array {
+    public static void main(String[] args) {
+
+        // 2차원 배열
+        int[][]arr = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+
+        // 최소직사각형
+//        for(int[] i : arr){
+//            System.out.println(Arrays.deepToString(arr));
+//        }
+
+
+
+        System.out.println(arr[0][1]);
+    }
+}
